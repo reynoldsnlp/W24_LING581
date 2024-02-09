2c67d5a924a44511a3e528c90d9cfaef917c30e7
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2024-02-09 14:50:58 -0700

    Tweak 7a template

--- assignments/7a_neural_LM_template.py
+++ assignments/7a_neural_LM_template.py
@@ -8,56 +8,19 @@ Bengio's original paper: http://www.jmlr.org/papers/volume3/bengio03a/bengio03a.
 from collections import Counter
 import multiprocessing
 from pprint import pprint
+import time
 
 import nltk
 from nltk.corpus import brown
 import numpy as np
 import torch
 from torch import nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
 
-nltk.download("brown")
-
-UNK_symbol = "<UNK>"
-gpu = 0
-NGRAM_SIZE = 3
-CONTEXT_SIZE = NGRAM_SIZE - 1
-EMBEDDING_DIM = 100
-HIDDEN_SIZE = 100
-BATCH_SIZE = 256
-EPOCHS = 5
-torch.manual_seed(42)
-
-
-# ### Build vocabulary ###
-
-# training set: first 12000 paragraphs (dev set: remaining 3000+)
-TRAIN_PARAGRAPHS = 12000
-
-train_freq = Counter()
-for i, paragraph in enumerate(brown.paras()):
-    if i == TRAIN_PARAGRAPHS:
-        break
-    for sentence in paragraph:
-        train_freq.update(word.lower() for word in sentence)
-print(train_freq.most_common(200))
-
-# create vocabulary
-vocab = set([UNK_symbol])
-for word, freq in train_freq.items():
-    if freq >= 5:
-        vocab.add(word)
-
-print("Vocab length:", len(vocab))
-
-word_to_id_mappings = {}
-for i, word in enumerate(vocab):
-    word_to_id_mappings[word] = i
-
-
-def get_id_of_word(word, unk=UNK_symbol):
+def get_id_of_word(word, unk="<UNK>"):
     """Get id of given word (<UNK> if not found)."""
     unknown_word_id = word_to_id_mappings[unk]
     return word_to_id_mappings.get(word, unknown_word_id)
@@ -89,25 +52,6 @@ def get_train_dev(corpus, ngram_size=3):
     return np.array(x_train), np.array(y_train), np.array(x_dev), np.array(y_dev)
 
 
-# ### Create DataLoaders ###
-
-# check if gpu is available
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# device = 'cpu'
-available_workers = multiprocessing.cpu_count()
-
-x_train, y_train, x_dev, y_dev = get_train_dev(brown, ngram_size=3)
-print("Train shape of x and y:", x_train.shape, y_train.shape)
-print("Dev shape of x and y:", x_dev.shape, y_dev.shape)
-
-
-print(f"--- Creating training and dev dataloaders with {BATCH_SIZE} batch size ---")
-train_set = np.concatenate((x_train, y_train), axis=1)
-dev_set = np.concatenate((x_dev, y_dev), axis=1)
-train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, num_workers=available_workers)
-dev_loader = DataLoader(dev_set, batch_size=BATCH_SIZE, num_workers=available_workers)
-
-
 class NPLM(nn.Module):
     """Neural Probabilistic Language Model (Bengio 2003)."""
 
@@ -116,67 +60,165 @@ class NPLM(nn.Module):
         # TODO build neural network
 
     def forward(self, inputs):
-        pass  # TODO compute feedforward step and return log probabilities
+        # TODO compute feedforward step and return log probabilities (use log_softmax)
 
 
-# ------------------------- TRAIN & SAVE MODEL ------------------------
-
-# Using negative log-likelihood loss
-loss_function = nn.NLLLoss()
-
-# create model
-model = NPLM(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE, HIDDEN_SIZE)
-
-# load it to gpu
-model.cuda(gpu)
-
-# using ADAM optimizer for learning rates
-optimizer = optim.Adam(model.parameters(), lr=2e-3)
+def get_accuracy_from_log_probs(log_probs, labels):
+    """Get accuracy from log probabilities"""
+    probs = torch.exp(log_probs)
+    predicted_label = torch.argmax(probs, dim=1)
+    acc = (predicted_label == labels).float().mean()
+    return acc
 
 
-for epoch in range(EPOCHS):
-    pass
-    # TODO Write the training loop
-    # TODO At the end of each 500 mini-batches...
-    #          * ...print the batch number and loss
-    # TODO At the end of each epoch...
-    #          * ...evaluate the model on the dev set and print results
-    #          * ...save the model to a file
-    #          * ...keep track of the best model's path in best_model_path
+def evaluate(model, criterion, dataloader, gpu):
+    """Evaluate model on dev data"""
+    model.eval()
 
-# ---------------------- Loading Best Model -------------------
+    mean_acc, mean_loss = 0, 0
+    count = 0
 
-best_model = NPLM(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE, HIDDEN_SIZE)
-best_model.load_state_dict(torch.load(best_model_path))
-best_model.cuda(gpu)
+    with torch.no_grad():
+        dev_st = time.time()
+        for it, data_tensor in enumerate(dataloader):
+            context_tensor = data_tensor[:, 0:CONTEXT_SIZE]
+            target_tensor = data_tensor[:, CONTEXT_SIZE]
+            context_tensor, target_tensor = context_tensor.to(device), target_tensor.to(device)
+            log_probs = model(context_tensor)
+            mean_loss += criterion(log_probs, target_tensor).item()
+            mean_acc += get_accuracy_from_log_probs(log_probs, target_tensor)
+            count += 1
+            if it % 500 == 0:
+                print(f"Dev Iteration {it} complete. "
+                      f"Mean Loss: {mean_loss / count}; "
+                      f"Mean Acc:{mean_acc / count}; "
+                      f"Time taken (s): {time.time()-dev_st}")
+                dev_st = time.time()
 
-cos = nn.CosineSimilarity(dim=0)
+    return mean_acc / count, mean_loss / count
 
-lm_similarities = {}
-# word pairs to calculate similarity
-words = [
-         ('computer', 'keyboard'),
-         ('war', 'peace'),
-         ('war', 'about'),
-         ('school', 'college'),
-         ('school', 'who'),
-         ('national', 'international'),
-         ('national', 'few'),
-        ]
 
-# ----------- Calculate LM similarities using cosine similarity ----------
+if __name__ == "__main__":
+    nltk.download("brown")
 
-for word_pair in words:
-    w1, w2 = word_pair
-    words_tensor = torch.LongTensor([get_id_of_word(w1), get_id_of_word(w2)])
-    words_tensor = words_tensor.cuda(gpu)
-    # get word embeddings from the best model
-    words_embeds = best_model.embeddings(words_tensor)
-    # calculate cosine similarity between word vectors
-    sim = cos(words_embeds[0], words_embeds[1])
-    lm_similarities[word_pair] = sim.item()
+    UNK_symbol = "<UNK>"
+    gpu = 0
+    NGRAM_SIZE = 3
+    CONTEXT_SIZE = NGRAM_SIZE - 1
+    EMBEDDING_DIM = 100
+    HIDDEN_SIZE = 100
+    BATCH_SIZE = 256
+    EPOCHS = 5
+    torch.manual_seed(42)
 
-print("\n\nSimilarity scores:")
-pprint(lm_similarities)
 
-# TODO Play with hyperparameters at the top of the script to see if you can improve these similarities
+    ### Build vocabulary ###
+
+    # training set: first 12000 paragraphs (dev set: remaining 3000+)
+    TRAIN_PARAGRAPHS = 12000
+
+    train_freq = Counter()
+    for i, paragraph in enumerate(brown.paras()):
+        if i == TRAIN_PARAGRAPHS:
+            break
+        for sentence in paragraph:
+            train_freq.update(word.lower() for word in sentence)
+    print(train_freq.most_common(200))
+
+    # create vocabulary
+    vocab = set([UNK_symbol])
+    for word, freq in train_freq.items():
+        if freq >= 5:
+            vocab.add(word)
+
+    print("Vocab length:", len(vocab))
+
+    word_to_id_mappings = {}
+    for i, word in enumerate(vocab):
+        word_to_id_mappings[word] = i
+
+
+    ### Create DataLoaders ###
+
+    # check if gpu is available
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    available_workers = multiprocessing.cpu_count()
+
+    x_train, y_train, x_dev, y_dev = get_train_dev(brown, ngram_size=3)
+    print("Train shape of x and y:", x_train.shape, y_train.shape)
+    print("Dev shape of x and y:", x_dev.shape, y_dev.shape)
+
+
+    print(f"--- Creating training and dev dataloaders with {BATCH_SIZE} batch size ---")
+    train_set = np.concatenate((x_train, y_train), axis=1)
+    dev_set = np.concatenate((x_dev, y_dev), axis=1)
+    train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, num_workers=available_workers)
+    dev_loader = DataLoader(dev_set, batch_size=BATCH_SIZE, num_workers=available_workers)
+
+
+    # ------------------------- TRAIN & SAVE MODEL ------------------------
+
+    # Using negative log-likelihood loss
+    loss_function = nn.NLLLoss()
+
+    # create model
+    model = NPLM(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE, HIDDEN_SIZE)
+
+    model.to(device)
+
+    # using ADAM optimizer
+    optimizer = optim.Adam(model.parameters(), lr=2e-3)
+
+    best_acc = 0
+    best_model_path = None
+    for epoch in range(EPOCHS):
+        print(f"\n--- Training model Epoch: {epoch+1} ---")
+        for it, data_tensor in enumerate(train_loader):
+            st = time.time()  # start time  (use time.time() - st to get time taken per minibatch)
+            context_tensor = data_tensor[:, 0:CONTEXT_SIZE]
+            target_tensor = data_tensor[:, CONTEXT_SIZE]
+            context_tensor, target_tensor = context_tensor.to(device), target_tensor.to(device)
+
+            # TODO Write the training loop
+            # TODO At the end of each 500 mini-batches...
+            #          * ...print the batch number and loss
+        # TODO At the end of each epoch...
+        #          * ...evaluate the model on the dev set and print results
+        #          * ...save the model to a file
+        #          * ...keep track of the best model's path in best_model_path
+
+
+    # ---------------------- Loading Best Model -------------------
+
+    best_model = NPLM(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE, HIDDEN_SIZE)
+    best_model.load_state_dict(torch.load(best_model_path))
+    best_model.to(device)
+
+    cos = nn.CosineSimilarity(dim=0)
+
+    lm_similarities = {}
+    # word pairs to calculate similarity
+    words = [
+             ('computer', 'keyboard'),
+             ('war', 'peace'),
+             ('war', 'about'),
+             ('school', 'college'),
+             ('school', 'who'),
+             ('national', 'international'),
+             ('national', 'few'),
+            ]
+
+    # ----------- Calculate LM similarities using cosine similarity ----------
+
+    for word_pair in words:
+        w1, w2 = word_pair
+        words_tensor = torch.LongTensor([get_id_of_word(w1), get_id_of_word(w2)])
+        words_tensor = words_tensor.to(device)
+        # get word embeddings from the best model
+        words_embeds = best_model.embeddings(words_tensor)
+        # calculate cosine similarity between word vectors
+        sim = cos(words_embeds[0], words_embeds[1])
+        lm_similarities[word_pair] = sim.item()
+
+    print("\n\nSimilarity scores:")
+    pprint(lm_similarities)
